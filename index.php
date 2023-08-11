ee1e6de618cf992f15a9586cac2fe66bd1050dd8
michaelmanaloee <gerhardmichael002@gmail.com>
2023-08-12 00:31:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="index.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+
+    <title>My Portfolio</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row py-4">
+            <div class="col-12 col-md-4 col-lg-4">
+          
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+              <div class="btn-collapse d-flex justify-content-center">
+                <button type="button" class="btn btn-title px-5" data-bs-toggle="collapse" href="#socialMedia" role="button" aria-expanded="false" aria-controls="socialMedia">
+                  <span>My Portfolio</span>
+                </button>
+              </div>
+              <div class="collapse mt-3 text-center" id="socialMedia">
+                  <a href="#"><i class="fab fa-github fa-lg px-1"></i></a>
+                  <a href="https://www.linkedin.com/in/michael-gerhard-manalu/"><i class="fab fa-linkedin fa-lg px-1"></i></a>
+                  <a href="https://www.instagram.com/michaelmanaloee/"><i class="fab fa-instagram fa-lg px-1"></i></a>
+                  <a href="https://youtube.com/@michaelmanaloee"><i class="fab fa-youtube fa-lg px-1"></i></a>
+              </div>            
+            </div>
+            <div class="col-12 col-md-4 col-lg-4">
+                 
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid py-3 px-lg-5 px-5">
+        <div class="row row-profile py-5 px-lg-5 px-2 d-flex align-items-center">
+            <div class="col-12 col-md-4 col-lg-4 d-flex justify-content-center">
+                <img class="img-responsive" src="DSCF1069.png" width="60%" height="auto"> 
+            </div>
+            <div class="col-12 col-md-8 col-lg-8">
+              <h5 class="text-white text-lg-start text-center">Hello World!</h5>
+              <h1 class="text-lg-start text-center">My name is Michael Gerhard Manalu.</h1>
+              <p class="text-white text-start">
+                A fresh graduate from the University of North Sumatera with a Diploma degree in Informatics Engineering. 
+                I am habituated to making plans before taking action so that I can accomplish tasks better. 
+                Able to do Web Programming, Python, and I also have expertise in various aspects of design, thats why I can use Adobe Tools (Photoshop, Illustrator, and Premier Pro). 
+                I am very interested in a career in Information Technology, especially as a Front-End Developer.</p>
+                <a href="CV_MICHAEL GERHARD MANALU.pdf" download>
+                  <button type="button" class="btn btn-download shadow-sm mt-2">
+                    Download CV
+                  </button>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid px-4 px-lg-3 py-0 py-lg-3 mb-2">
+      <div class="row d-flex justify-content-xxl-evenly align-items-strech px-lg-0 px-4">
+        <div class="col-12 col-md-5 col-lg-5 col-skill py-4 px-4 mt-lg-0 mt-4 mb-lg-0 mb-3">
+          <h5 class="text-center mb-4">My Skills & Tools</h5>
+          <div class="owl-carousel owl-theme text-center">
+              <div class="item"><p>HTML</p></div>
+              <div class="item"><p>CSS</p></div>
+              <div class="item"><p>PHP</p></div>
+              <div class="item"><p>Bootstrap</p></div>
+              <div class="item"><p>MySQL</p></div>
+              <div class="item"><p>Python</p></div>
+              <div class="item"><p>Adobe Photoshop</p></div>
+              <div class="item"><p>Adobe Illustrator</p></div>
+              <div class="item"><p>Adobe Premier Pro</p></div>
+          </div>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3 col-website py-4 py-lg-5 mb-lg-0 mb-3">
+          <div class="container">
+            <div class="row d-flex align-items-center">
+              <div class="col-6 col-md-6 col-lg-6 d-flex justify-content-center">
+                <h2>See my Websites</h2>
+              </div>
+              <div class="col-6 col-md-6 col-lg-6 d-flex justify-content-center">
+                <a href="" data-bs-toggle="modal" data-bs-target="#myWebsites"><i class="fas fa-chevron-circle-right fa-lg"></i></a>
+
+                <div class="modal fade" id="myWebsites" tabindex="-1" aria-labelledby="myWebsitesLabel" aria-hidden="true">
+                  <div class="modal-dialog modal-fullscreen">
+                    <div class="modal-content modal-website shadow-sm px-1 py-1">
+                      <div class="modal-header px-5">
+                        <h5 class="modal-title py-3" id="myWebsitesLabel">My Website</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        <div class="container-fluid">
+                          <div class="row px-0 d-flex justify-content-center">
+                            <div class="col-12 col-md-6 col-lg-6">
+                              <div class="card card-web1 shadow-sm">
+                                <img src="landing page wl.png" class="card-img-top py-2 px-2" alt="...">
+                                <div class="card-body">
+                                  <p class="card-text">Waitinglist Coffee & Tea</p>
+                                </div>
+                                <a href="http://waitinglistcoffeeandtea.infinityfreeapp.com/"><div class="card-footer"><small>Lets See!</small></div></a>
+                              </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6 mt-lg-0 mt-4">
+                              <div class="card card-web2 shadow-sm">
+                                <img src="landing page kfn.png" class="card-img-top py-2 px-2" alt="...">
+                                <div class="card-body">
+                                <p class="card-text">Klinik Fisioterapi Ngawi</p>
+                                </div>
+                                <a href="http://ngawi.epizy.com/"><div class="card-footer"><small>Lets See!</small></div></a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3 col-design py-4 py-lg-5 mb-lg-0 mb-3">
+          <div class="container">
+            <div class="row d-flex align-items-center">
+              <div class="col-6 col-md-6 col-lg-6 d-flex justify-content-center">
+                <h2>See my Designs</h2>
+              </div>
+              <div class="col-6 col-md-6 col-lg-6 d-flex justify-content-center">
+                <a href="" data-bs-toggle="modal" data-bs-target="#myDesigns"><i class="fas fa-chevron-circle-right fa-lg"></i></a>
+                  
+                  <div class="modal fade" id="myDesigns" tabindex="-1" aria-labelledby="myDesignsLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen">
+                      <div class="modal-content modal-designs">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="myDesignsLabel">My Designs</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="container">
+                            <div class="row">
+                              <div class="col-12 col-md-4 col-lg-4">
+                                <img src="designs/Hari Buruh Flyer MILIK MICHAEL GERHARD MANALU.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/Jumat Agung.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/Logo Klinik-01.png" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/mylogo.png" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/ourbanner.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/POLAROID MASIH KEREN 1080 x 1350 JASMINE RIGHT NEW.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/Poster Rutan Nina REV.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/sharper regrert.jpg" alt="" class="img-fluid">
+                              </div>
+                              <div class="col-12 col-md-4 col-lg-4 py-lg-0 py-2 d-flex align-items-center">
+                                <img src="designs/Template daily report.jpg" alt="" class="img-fluid">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="text/javascript">
+      $('.owl-carousel').owlCarousel({
+          loop:false,
+          autoplay:true,
+          autoplayHoverPause:true,
+          margin:10,
+          nav:true,
+          dots:false,
+          responsive:{
+              0:{
+                  items:3
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:4
+              }
+          }
+      })
+    </script>
+    
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
